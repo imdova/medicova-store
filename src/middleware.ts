54c27113c2b598e781e9367cb5eeea58b2208ee9
Comatemplets <zezo7817289@gmail.com>
2025-05-27 21:01:35 +0300

    fix debugs

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,5 +21,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: ["/checkout", "/checkout/:path*"], // Protect all checkout routes
+  matcher: ["/login", "/checkout/:path*"], // Protect all checkout routes
 };
